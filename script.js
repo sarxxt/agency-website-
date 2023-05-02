decb86bc9f9d210b9251eb131362da2375645b4e
sarxxt <83841462+sarxxt@users.noreply.github.com>
2023-05-03 03:04:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const toggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
+
+toggle.addEventListener('click', function()
+{ 
+    toggle.classList.toggle('active');
+    navigation.classList.toggle("active");
+
+})
